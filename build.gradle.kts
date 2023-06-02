31eca25f05a4eea734b60122bde50cd78b95567a
Jeremy Horwitz <jhmail@gmail.com>
2023-06-01 18:29:46 -0700

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.20"
     id("com.github.ben-manes.versions") version "0.46.0"
-    id("com.diffplug.spotless") version "6.18.0"
+    id("com.diffplug.spotless") version "6.19.0"
     application
 }
 

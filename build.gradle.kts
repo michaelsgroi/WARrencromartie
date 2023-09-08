e6bff1084945b2b2b315eef84dc21d9f95dbc53e
Jeremy Horwitz <jhmail@gmail.com>
2023-09-07 20:25:24 -0700

    version bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.21"
-    id("com.github.ben-manes.versions") version "0.46.0"
-    id("com.diffplug.spotless") version "6.19.0"
+    kotlin("jvm") version "1.9.10"
+    id("com.github.ben-manes.versions") version "0.48.0"
+    id("com.diffplug.spotless") version "6.21.0"
     application
 }
 
@@ -15,7 +15,7 @@ repositories {
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
 }
 
 tasks.test {
